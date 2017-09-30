28ed9950824842082e3b0b515ee13429d98193f7
Woojin <Woojin@Woojin-PC>
2017-09-30 23:00:32 +0900

    테스트 커밋

---
+++ hajiboot/src/main/java/com/example/hajiboot/App.java
@@ -0,0 +1,26 @@
+package com.example.hajiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App 
+{
+	@RequestMapping("/test")	// 이 메서드가 http요청을 받아들이는 메서드
+	String home(){
+		return "hellooooo worldd22334455";
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
